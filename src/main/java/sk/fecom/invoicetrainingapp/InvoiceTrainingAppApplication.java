edde18e03a58ae29d59bdbb4472d70ea1bec9a0d
Marek FECO <marek.feco@ibm.com>
2019-05-30 20:42:50 +0200

    #1 initial version

---
+++ src/main/java/sk/fecom/invoicetrainingapp/InvoiceTrainingAppApplication.java
@@ -0,0 +1,13 @@
+package sk.fecom.invoicetrainingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceTrainingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceTrainingAppApplication.class, args);
+	}
+
+}
